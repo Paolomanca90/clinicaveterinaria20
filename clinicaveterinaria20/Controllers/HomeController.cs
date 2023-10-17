972e9faf431fdfd3be709e71783a2aa442124eaf
maicol abbiento <128224384+MaicolAbbiento@users.noreply.github.com>
2023-10-17 10:04:54 +0200

    ok

--- clinicaveterinaria20/Controllers/HomeController.cs
+++ clinicaveterinaria20/Controllers/HomeController.cs
@@ -1,18 +1,46 @@
-﻿using System;
+﻿using clinicaveterinaria20.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace clinicaveterinaria20.Controllers
 {
     public class HomeController : Controller
     {
+        private Model1 database = new Model1();
+
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
 
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult login(Utente u)
+        {
+            Utente utente = database.Utente.FirstOrDefault((e) => e.username == u.username && e.password == u.password);
+            if (utente != null)
+            {
+                FormsAuthentication.SetAuthCookie(u.username, false);
+            }
+            return View();
+        }
+
+        public ActionResult logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
         }
     }
 }
---
+++ clinicaveterinaria20/Models/SiteRole.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace clinicaveterinaria20.Models
+{
+    public class SiteRole : RoleProvider
+    {
+        public override string ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetRolesForUser(string username)
+        {
+            Model1 model = new Model1();
+            Utente cl = model.Utente.FirstOrDefault(p => p.username == username);
+            List<string> roles = new List<string>();
+            if (cl != null)
+            {
+                roles.Add(cl.role);
+            }
+            return roles.ToArray();
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
