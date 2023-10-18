9ed7a06410cd6ab0bbf5732b04b5fc1b973e6001
maicol abbiento <128224384+MaicolAbbiento@users.noreply.github.com>
2023-10-18 11:56:03 +0200

    ok

---
+++ clinicaveterinaria20/Migrations/202310180953269_rejex.cs
@@ -0,0 +1,18 @@
+﻿namespace clinicaveterinaria20.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class rejex : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.Armadietti", "codice");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Armadietti", "codice", c => c.String(nullable: false));
+        }
+    }
+}
