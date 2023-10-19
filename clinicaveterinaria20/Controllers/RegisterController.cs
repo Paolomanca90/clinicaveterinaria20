87fc91ed12fd0b15c2e0253e8d7203d2c80680e5
ValerioParatore <ValerioTeoParatore@gmail.com>
2023-10-19 10:38:07 +0200

    register

---
+++ clinicaveterinaria20/Controllers/RegisterController.cs
@@ -0,0 +1,37 @@
+﻿using clinicaveterinaria20.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace clinicaveterinaria20.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        private Model1 db = new Model1();
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register (Utente u)
+        {
+            List<Utente> uList = db.Utente.ToList();
+            Utente utente = db.Utente.FirstOrDefault(m=> m == u);
+            if (utente == null)
+            {
+                db.Utente.Add(u);
+                db.SaveChanges();
+            }
+            else
+            {
+                ViewBag.Errore = "Utente presente nel database";
+            }
+
+            return View();
+        }
+    }
+}
