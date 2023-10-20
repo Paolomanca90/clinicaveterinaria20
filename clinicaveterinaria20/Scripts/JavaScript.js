cd33ef686f748f9c39ef32986ee31bd324e9b821
ValerioParatore <ValerioTeoParatore@gmail.com>
2023-10-20 11:18:13 +0200

    welelel

--- clinicaveterinaria20/Scripts/JavaScript.js
+++ clinicaveterinaria20/Scripts/JavaScript.js
@@ -19,11 +19,11 @@
                                 p = `<p> ${e.nome} prodotto mometaneamente non presente </p>`
                             }
                             else {
-                                p = `<p class="m-2"> ci sono ${e.quantita} ${e.nome} con un costo ${e.costo}€ sitati nel armadietto n${e.armadietto} nel cassetto${e.casetto} <a href="./modificaprodotto/${e.idprodotto}" class="btn btn-light rounded-pill border-0 w-25 fw-semibold p-2"> modifica prodotto</a> <a href="./eliminaprodotto/${e.idprodotto}" class="btn btn-light rounded-pill border-0 w-25 fw-semibold p-2"> elimina dalla vendita </a> </p>
+                                p = `<p class="m-2"> ci sono ${e.quantita} ${e.nome} con un costo ${e.costo}€ sitati nel armadietto n${e.armadietto} nel cassetto${e.casetto} <a href="./modificaprodotto/${e.idprodotto}"> modifica prodotto</a> - <a href="./eliminaprodotto/${e.idprodotto}" > elimina dalla vendita </a> </p>
                                         <img src="/Content/img/uploads/${e.foto}"  class="rounded-circle img-magazzino"/>`
                             }
                         }
-                        else { p = `<p class="m-2"> ${e.nome} prodotto non più in vendita <a href="./modificaprodotto/${e.idprodotto}" class="btn btn-light rounded-pill border-0 w-25 fw-semibold p-2"> modifica prodotto</a> <a href="./eliminaprodotto/${e.idprodotto}"> rimetti in vendita</a>  </p>  <img src="/Content/img/uploads/${e.foto}"  class="rounded-circle img-magazzino" />` }
+                        else { p = `<p class="m-2"> ${e.nome} prodotto non più in vendita <a href="./modificaprodotto/${e.idprodotto}" > modifica prodotto</a> - <a href="./eliminaprodotto/${e.idprodotto}"> rimetti in vendita</a>  </p>  <img src="/Content/img/uploads/${e.foto}"  class="rounded-circle img-magazzino" />` }
                     }
                     $("#div").append(p)
                 })
