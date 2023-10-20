cd521f51b09905afa1a5ba5a40db21884f1693c4
Raffaele Ianniello <134380099+RaffaeleIanniello@users.noreply.github.com>
2023-10-20 16:27:30 +0200

    LastDance

--- clinicaveterinaria20/Models/Brand.cs
+++ clinicaveterinaria20/Models/Brand.cs
@@ -19,19 +19,19 @@ namespace clinicaveterinaria20.Models
         public int idbrand { get; set; }
 
         [Required(ErrorMessage = "campo richiesto")]
-        [Display(Name = "Inserisci il nome del Brand")]
+        [Display(Name = "Nome Brand")]
         public string nome { get; set; }
 
         [StringLength(11)]
-        [Display(Name = "Inserisci la Partita Iva")]
+        [Display(Name = "Partita Iva")]
         public string piva { get; set; }
 
         [StringLength(56)]
-        [Display(Name = "Inserisci la Sede Legale del Brand")]
+        [Display(Name = "Sede Legale")]
         public string sedelegale { get; set; }
 
         [StringLength(15)]
-        [Display(Name = "Inserisci il recapito telefonico")]
+        [Display(Name = "Recapito Telefonico")]
         public string recapito { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
